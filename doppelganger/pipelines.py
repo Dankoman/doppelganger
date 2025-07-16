ce86654433fd43ea14569c867c4cc1027341750d
marqs <markus.rohsto@gmail.com>
2025-07-16 23:56:05 +0200

    kaka

--- doppelganger/pipelines.py
+++ doppelganger/pipelines.py
@@ -9,6 +9,6 @@ class PerformerImagePipeline(ImagesPipeline):          # <-- namn matchar spider
         Spara varje bild som <sanerat_namn>/<originalfil>.jpg
         """
         performer = ItemAdapter(item).get("performer", "unknown")
-        safe_dir  = re.sub(r"[^a-zA-Z0-9_-]", "_", performer)
+        safe_dir  = re.sub(r"[^a-zA-Z0-9 -]", "_", performer)
         image_name = request.url.split("/")[-1]
         return f"{safe_dir}/{image_name}"
